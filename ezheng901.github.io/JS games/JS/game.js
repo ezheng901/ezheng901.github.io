b72e714682756f1d270209f9e770be4f4980f03f
ezheng901 <eddysuper3000@gmail.com>
2023-05-12 01:27:05 -0700

    Finialing pages

--- ezheng901.github.io/JS games/JS/game.js
+++ ezheng901.github.io/JS games/JS/game.js
@@ -1,72 +1,50 @@
-//this file is for code needed for input, quiz, and truth game. 
-
-//global variable for first name, in other owrds, variables that can be used in all functions 
-
-
-let fname = ""
-
-//function to get info from intro form
-//check input and produce a sentence 
-function greet(){
-
+// this file is for code needed for
+// input, quiz and truth game
+// global variable for first name
+// a variable that can be used in all functions
+// first name
+let fname = "";
+// function to get info from intro form
+// check input, and produce a sentence
+function greet() {
     let greetParagraph = document.getElementById("greet");
-
-    // get the value from the form
-
+    // get the values from the form
     fname = document.getElementById("fname").value;
     let lname = document.getElementById("lname").value;
     let age = document.getElementById("age").value;
-
-    //check the input 
-    if (fname==="" || lname=== ""){
-        alert("First and last name cannot be an empty string!")
+    // check the input
+    if (fname === "" || lname === "") {
+        alert("First and last name cannot be an empty string!");
+        return;
     }
-
-    //age check
-    if (age ==="" || age < 18){
-    alert("Invalid age! You must be 18 or older!");
-    return;
+    // age check
+    if (age === "" || age < 18) {
+        alert("Invalid age! You must be 18 or older!")
+        return;
     }
-    
-    //update output paragraph
-    greetParagraph.innerHTML = " Greetings " + age + " year old " + fname + "!";
+    // update output paragraph
+    greetParagraph.innerHTML = "Greetings " + age + " year old " + fname + "!";
 }
-
-// function to play the trivia game 
-function trivia1(){
+// function to play the trivia game
+function trivia1() {
     // get handle to answer paragraph
     let triviaAnswer = document.getElementById("trivia-answer");
-
-    //get handles and values from radio selections
-
+    // get handles and values from radio selections
     let chocSelected = document.getElementById("chocolate").checked;
     let tunaSelected = document.getElementById("tuna").checked;
     let honeySelected = document.getElementById("honey").checked;
-
-    //give the results
-
-    if (chocSelected){
-        triviaAnswer.innerHTML = fname + ", tuna is wrong";
-
+    // give the results
+    if (chocSelected) {
+        triviaAnswer.innerHTML = fname + " I indeed have eaten an ant. It tasted spicy!";
     }
-
-    else if (chocSelected) {
-        triviaAnswer.innerHTML = fname + ", chocolate is wrong!";
+    else if (tunaSelected) {
+        triviaAnswer.innerHTML = fname + " Unfortunately, this did happen to me, and I spent over three days in the hospital!";
     }
-
     else if (honeySelected) {
-        triviaAnswer.innerHTML = nrame + ", honey is right!";
-
-
+        triviaAnswer.innerHTML = fname + " Bravo! You are right! I love the Cars series!";
     }
-
     else {
-        triviaAnswer.innerHTML = "uh oh, something went wrong!!!";
+        triviaAnswer.innerHTML = "Uh oh. Something went wrong!!!";
     }
-
-    
-
-
-
-
 }
+
